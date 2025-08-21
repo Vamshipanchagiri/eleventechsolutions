346d913ece7ba1399948d685dbfbea903490f8db
elevantechhr <elevantechhr@gmail.com>
2025-08-21 16:02:24 +0530

    Fix HomePage imports

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,30 +1,22 @@
-import React from 'react';
-import Home from '@/pages/Home';
-import Services from '@/pages/Services';
-import WhyUs from '@/pages/WhyUs';
-import Clients from '@/pages/Clients';
-import Contact from '@/pages/Contact';
+import React from "react";
+import Home from "./home.jsx";
+import About from "./about.jsx";
+import Clients from "./clients.jsx";
+import Contact from "./contact.jsx";
+import Services from "./services.jsx";
+import WhyUs from "./whyus.jsx";
 
-const HomePage = () => {
+function HomePage() {
   return (
-    <div>
-      <div id="home">
-        <Home />
-      </div>
-      <div id="services">
-        <Services />
-      </div>
-      <div id="why-us">
-        <WhyUs />
-      </div>
-      <div id="clients">
-        <Clients />
-      </div>
-      <div id="contact">
-        <Contact />
-      </div>
+    <div className="home-page">
+      <Home />
+      <About />
+      <Services />
+      <Clients />
+      <WhyUs />
+      <Contact />
     </div>
   );
-};
+}
 
 export default HomePage;
