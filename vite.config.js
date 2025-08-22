942a89398077deb20c98a48ddad0986e622065e8
elevantechhr <elevantechhr@gmail.com>
2025-08-22 13:45:27 +0530

    Fix Vite EmailJS build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path'; // <-- add this
 
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: ["@emailjs/browser"]
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src') // <-- tells Vite @ = src
+    }
   }
 });
