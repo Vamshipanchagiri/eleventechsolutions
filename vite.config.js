f4ee2019278cc5026c7aa1b159f208bfb1868680
Vamshipanchagiri <panchagirivamshikrishna@gmail.com>
2025-08-22 13:25:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import sitemap from "vite-plugin-sitemap";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    sitemap({
-      hostname: "https://eleventechsolutions.in",
-      outDir: "dist",
-      urls: [
-        "/", 
-        "/about",
-        "/contact",
-        "/services",
-      ],
-    }),
-  ],
-  build: {
-    outDir: "dist",
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
+  plugins: [react()],
+  optimizeDeps: {
+    include: ["@emailjs/browser"]
+  }
 });
